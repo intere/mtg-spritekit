400c60f41f9565710729a13b3af9c296bfbabdf8
Eric Internicola <intere@gmail.com>
2018-09-16 20:07:32 -0600

    Following the tutorial on https://www.raywenderlich.com/1062-card-game-mechanics-in-sprite-kit-with-swift.  So far, I'm up to where it wiggles.

---
+++ CardGameV2/Card.swift
@@ -0,0 +1,65 @@
+/**
+ * Copyright (c) 2016 Razeware LLC
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+
+import SpriteKit
+
+enum CardType :Int {
+    case wolf,
+    bear,
+    dragon
+}
+
+class Card : SKSpriteNode {
+    let cardType :CardType
+    let frontTexture :SKTexture
+    let backTexture :SKTexture
+    var damage = 0
+    let damageLabel :SKLabelNode
+
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("NSCoding not supported")
+    }
+
+    init(cardType: CardType) {
+        self.cardType = cardType
+        backTexture = SKTexture(imageNamed: "card_back")
+
+        switch cardType {
+        case .wolf:
+            frontTexture = SKTexture(imageNamed: "card_creature_wolf")
+        case .bear:
+            frontTexture = SKTexture(imageNamed: "card_creature_bear")
+        case .dragon:
+            frontTexture = SKTexture(imageNamed: "card_creature_dragon")
+        }
+
+        damageLabel = SKLabelNode(fontNamed: "OpenSans-Bold")
+        damageLabel.name = "damageLabel"
+        damageLabel.fontSize = 12
+        damageLabel.fontColor = SKColor(red: 0.47, green: 0.0, blue: 0.0, alpha: 1.0)
+        damageLabel.text = "0"
+        damageLabel.position = CGPoint(x: 25, y: 40)
+
+        super.init(texture: frontTexture, color: .clear, size: frontTexture.size())
+        addChild(damageLabel)
+    }
+}
---
+++ CardGameV2/GameScene.swift
@@ -0,0 +1,95 @@
+/**
+ * Copyright (c) 2016 Razeware LLC
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+
+import SpriteKit
+
+enum CardLevel :CGFloat {
+    case board = 10
+    case moving = 100
+    case enlarged = 200
+}
+
+class GameScene: SKScene {
+
+    override func didMove(to view: SKView) {
+        let bg = SKSpriteNode(imageNamed: "bg_blank")
+        bg.anchorPoint = CGPoint.zero
+        bg.position = CGPoint.zero
+        addChild(bg)
+
+        let wolf = Card(cardType: .wolf)
+        wolf.position = CGPoint(x: 100, y: 200)
+        addChild(wolf)
+
+        let bear = Card(cardType: .bear)
+        bear.position = CGPoint(x: 300, y: 200)
+        addChild(bear)
+    }
+
+    override func touchesMoved(_ touches: Set<UITouch>, with event: UIEvent?) {
+        for touch in touches {
+            let location = touch.location(in: self)           // 1
+            if let card = atPoint(location) as? Card {        // 2
+                card.position = location
+            }
+        }
+    }
+
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        for touch in touches {
+            let location = touch.location(in: self)
+            if let card = atPoint(location) as? Card {
+                card.zPosition = CardLevel.moving.rawValue
+                card.removeAction(forKey: "drop")
+                card.run(SKAction.scale(to: 1.3, duration: 0.25), withKey: "pickup")
+                wiggle(it: card)
+            }
+        }
+    }
+
+    override func touchesEnded(_ touches: Set<UITouch>, with event: UIEvent?) {
+        for touch in touches {
+            let location = touch.location(in: self)
+            if let card = atPoint(location) as? Card {
+                card.zPosition = CardLevel.board.rawValue
+                card.removeFromParent()
+                addChild(card)
+                card.removeAction(forKey: "pickup")
+                card.run(SKAction.scale(to: 1.0, duration: 0.25), withKey: "drop")
+                stopWiggle(it: card)
+            }
+        }
+    }
+
+    func wiggle(it card: Card) {
+        let wiggleIn = SKAction.scaleX(to: 1.0, duration: 0.2)
+        let wiggleOut = SKAction.scaleX(to: 1.2, duration: 0.2)
+        let wiggle = SKAction.sequence([wiggleIn, wiggleOut])
+
+        card.run(SKAction.repeatForever(wiggle), withKey: "wiggle")
+    }
+
+    func stopWiggle(it card: Card) {
+        card.removeAction(forKey: "wiggle")
+    }
+
+}
